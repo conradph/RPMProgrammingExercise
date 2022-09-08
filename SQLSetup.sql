c5d236c86376bfd5a94fdbdc1fb3d771073a6a6a
conradph <conradph@msu.edu>
2022-09-08 13:25:35 -0400

    SQL setup file

--- SQLSetup.sql
+++ SQLSetup.sql
@@ -1,14 +1,10 @@
 USE RPMProgrammingExercise;
 
-drop table GasPrices;
+--drop table GasPrices;
 
 CREATE TABLE GasPrices(
 	recordDate varchar(8),
 	price float
 )
 
-
 select * from GasPrices;
-
-insert into GasPrices
-values ('20220808', 6.99);
